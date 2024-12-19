bf05253962310985c899c792f4e30664ffb15c2b
Mounik <mounicou@gmail.com>
2024-12-19 01:58:35 +0100

    Corrections

--- generate_subdomains.sh
+++ generate_subdomains.sh
@@ -3,5 +3,5 @@ DOMAIN="${1}"
 FILE="${2}"
 # Read the file from standard input and echo the full domain.
 while read -r subdomain; do
- echo "${subdomain}.${DOMAIN}"
+  echo "${subdomain}.${DOMAIN}"
 done < "${FILE}"
--- multi-host-ping.sh
+++ multi-host-ping.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Generate IP addresses from a given range.
 for ip in $(seq 1 254); do
- echo "172.16.10.${ip}" >> 172-16-10-hosts.txt
+  echo "172.16.10.${ip}" >> 172-16-10-hosts.txt
 done
